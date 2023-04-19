04ced9630cba19cf63da5356a0beec1ef7ac6aea
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-04-19 18:00:43 +0300

    added game power button

--- src/App.js
+++ src/App.js
@@ -6,30 +6,69 @@ import Circles from './Components/Circles';
 
 
 class App extends Component {
+  
+  state ={
+    score: 0,
+    start: false,
+    startButton: false,
+    endButton: true,
+    disabled: true,
+    circle1: '',
+    circle2: '',
+    circle3: '',
+    circle4: ''
+  };
+  clickHandler = (e) => {
+    this.setState({
+      score: this.state.score+10
+    
+    });
+    console.log(e.target.name)
+    // e.target.disabled = true
+  }
+  powerHandler = (e) => {
+    if(e.target.name=='start'){
+      this.setState({
+        startButton: true,
+        endButton: false,
+        start: true,
+        disabled: false
+        // stop: false
+      })
+      console.log('on')
+    } else{
+      this.setState({
+        endButton: true,
+        startButton: false,
+        start: false,
+        disabled: true
+        
+
+      })
+      console.log('off')
+    }
+    
+    
+  }
   Button = () => {
       return(
         <div>
   
-        <button>Start</button>
-        <button>End</button>
+        <button name="start" disabled={this.state.startButton}onClick={this.powerHandler}>Start</button>
+        <button name="off" disabled={this.state.endButton} onClick={this.powerHandler}>End</button>
         </div>
   
       )
   }
 
-  clickHandler = () => {
-    console.log('i was clicked')
-  }
   render(){
 
     return (
       <div className="App">
         <h1>Speedgame</h1>
-        <p>Score: <span>0</span></p>
-        <div className='circles-container'>
+        <p>Score: <span>{this.state.score}</span></p>
 
-        <Circles clickHandler={this.clickHandler}/>
-        </div>
+        <Circles clickHandler={this.clickHandler} disabled={this.state.disabled}/>
         <this.Button />
        
       </div>
